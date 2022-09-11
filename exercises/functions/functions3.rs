4585043f82505b3b5d9a812536b37053990b56fe
toonarmycaptain <toonarmycaptain@hotmail.com>
2022-09-11 13:41:06 -0500

    Functions

--- exercises/functions/functions3.rs
+++ exercises/functions/functions3.rs
@@ -1,14 +1,13 @@
 // functions3.rs
 // Execute `rustlings hint functions3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    call_me();
+    call_me(23); // argument was not provided.                                        
 }
 
 fn call_me(num: u32) {
-    for i in 0..num {
+    for i in 0..num {                   
         println!("Ring! Call number {}", i + 1);
     }
 }
--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -1,7 +1,6 @@
 // variables6.rs
 // Execute `rustlings hint variables6` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 const NUMBER: i8 = 3; //constants must have their type declared
 fn main() {
